11fd47e0658a9ed598030a1d2e24ff11b7e5cb9f
funniray <funniray10@gmail.com>
2018-02-04 13:01:58 -0500

    Fixed order of buttons

--- src/main/java/me/thinkpuppy/emotes/inventory/EmotesGUI.java
+++ src/main/java/me/thinkpuppy/emotes/inventory/EmotesGUI.java
@@ -15,22 +15,19 @@ public class EmotesGUI {
     public static Inventory emoteGui;
 
     public EmotesGUI(){
-        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Eating Food",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is munching on some food. "+ ChatColor.GOLD+ ChatColor.BOLD+ "OM NOM NOM!", Material.COOKED_BEEF));
-        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Happy Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is happy, you're a great person for being there for them.",Material.DIAMOND_CHESTPLATE));
-        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Sad Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is currently sad, you should talk to them.",Material.DIAMOND_CHESTPLATE));
-        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Hug Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is putting their hands out for a hug.",Material.DIAMOND_CHESTPLATE));
-        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Dance Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Wants to dance, come out onto the dance floor with them!",Material.DIAMOND_CHESTPLATE));
-
         emoteGui = Bukkit.createInventory(null,9,"Emotes");
-        int i = 2;
-        for (ItemStack item:GuiItems.keySet()){
-            emoteGui.setItem(i,item);
-            i++;
-        }
+
+        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Happy Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is happy, you're a great person for being there for them.",Material.DIAMOND_CHESTPLATE),2);
+        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Sad Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is currently sad, you should talk to them.",Material.DIAMOND_CHESTPLATE),3);
+        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Eating Food",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is munching on some food. "+ ChatColor.GOLD+ ChatColor.BOLD+ "OM NOM NOM!", Material.COOKED_BEEF),4);
+        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Hug Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Is putting their hands out for a hug.",Material.DIAMOND_CHESTPLATE),5);
+        addGuiItem(new GuiItem(ChatColor.LIGHT_PURPLE+ "Dance Emote",ChatColor.LIGHT_PURPLE+ "%player%" + ChatColor.AQUA+ " Wants to dance, come out onto the dance floor with them!",Material.DIAMOND_CHESTPLATE),6);
     }
 
-    private void addGuiItem(GuiItem item){
-        GuiItems.put(item.getItem(),item);
+    private void addGuiItem(GuiItem item, int slot){
+        ItemStack itemStack = item.getItem();
+        GuiItems.put(itemStack,item);
+        emoteGui.setItem(slot,itemStack);
     }
 
     public GuiItem getGuiItem(ItemStack item){
